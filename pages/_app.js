6372cf7a48eef12747edb2f1ca252b8239b84e0e
cedarrival <cedarrival@foxmail.com>
2022-11-09 09:15:33 +0800

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import Head from 'next/head'
+import Nav from "../components/Nav";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+    return (
+        <>
+            <Head>
+                <title>Next and MDX Blog</title>
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+                      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+                      crossOrigin="anonymous"/>
+            </Head>
+
+            <div className="container">
+                <Nav/>
+                <main>
+                    <Component {...pageProps} />
+                </main>
+            </div>
+        </>
+    )
 }
 
 export default MyApp
---
+++ pages/bio.js
@@ -0,0 +1,13 @@
+const Bio = () => {
+  return (
+    <div className="mt-3">
+      <p className="display-4 text-center">I&#39;m Ebenezer Don</p>
+      <p className="text-center">Ebenezer Don&#39;s bio here</p>
+    </div>
+  )
+}
+
+
+
+
+export default Bio
